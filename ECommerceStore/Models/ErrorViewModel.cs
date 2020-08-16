e7a45903f9ef3fb9ebb5a3553152dba202163852
vepinch <zloyhomak@gmail.com>
2020-08-16 16:23:05 +0300

    Created ECommerceStore

---
+++ ECommerceStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ECommerceStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
